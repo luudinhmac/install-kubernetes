324ec90c990b4289ddf7ce67ace4c3975bc3b332
luudinhmac <nguoiemcuanui@gmail.com>
2023-03-01 16:41:21 +0700

    done

--- node.sh
+++ node.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # install containerd
+read -s -p "Enter Password for sudo: " sudoPW
+echo $sudoPW | sudo -S apt update
+
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-sudo modprobe overlay
+sudo -S modprobe overlay
 sudo modprobe br_netfilter
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-ip6tables = 1 
 EOF
 sudo sysctl --system
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt update -y
+echo $sudoPW | sudo -S apt update -y
 sudo apt install containerd.io -y
 
 sudo mkdir /etc/containerd
 containerd config default >/etc/containerd/config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-systemctl restart containerd
+sudo systemctl restart containerd
